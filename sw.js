04d25df95eca4f3e02e7babb1042bdca72c4a532
MEDEXPERT <THEONLYCREATIVEADS@GMAIL.COM>
2026-04-05 17:24:28 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'rkpi-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const c = r.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, c));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
